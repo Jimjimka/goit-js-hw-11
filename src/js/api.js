5f18017784b650d62a45f633cee133cc50d86e68
Jimjimka <106761382+Jimjimka@users.noreply.github.com>
2022-11-22 04:11:18 +0200

    Done

--- src/js/api.js
+++ src/js/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-BASE_URL = 'https://pixabay.com/api/'
-API_KEY = '31447309-c48142debbc2e62fc86ad26d0'
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '31447309-c48142debbc2e62fc86ad26d0';
 
 export class PixabayAPI{
   
